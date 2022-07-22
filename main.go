0ccb102273cdf8bb7d80af55b8680abc57c59e5a
Burhan Safran <burhan.safran@ecozum.com>
2022-07-22 21:43:34 +0300

    cors_domains splitted

--- main.go
+++ main.go
@@ -4,9 +4,8 @@ import (
 	"ToDo/configs"
 	"ToDo/db"
 	"ToDo/routes"
-	"fmt"
 	"log"
-	"time"
+	"os"
 
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/fiber/v2/middleware/cors"
@@ -16,10 +15,9 @@ import (
 func main() {
 	configs.SetEnv()
 	db.Init()
-	fmt.Println(500 * time.Millisecond)
 	app := fiber.New()
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "http://localhost:5500, http://127.0.0.1:5500",
+		AllowOrigins:     os.Getenv("CORS_DOMAINS"),
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization, X-Csrf-Token",
 		AllowCredentials: true,
 	}))
